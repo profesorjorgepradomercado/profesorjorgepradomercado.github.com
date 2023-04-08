35fdfaa15eaec215bb5c8db208102600e6d1e8c6
jorge_prado_mercado <pradomercadojorge@gmail.com>
2023-04-08 17:58:53 -0600

    subiendo proyecto de gasolineras

---
+++ proyecto2/javascript/main.js
@@ -0,0 +1,60 @@
+// let estrellas = document.querySelectorAll('i');
+// estrellas.forEach(function(e){
+//   e.classList.remove('fa-gas-pump');
+// });
+
+
+/*
+let botonesCerrar = document.querySelectorAll(".close");
+// console.log(botonesCerrar);
+botonesCerrar.forEach(function(boton){
+  boton.addEventListener('click', function(evento){
+    // window.alert("adios 2");  
+    // evento.preventDefault();
+    console.log("Adios");
+  });
+});
+*/
+
+
+// El código anterior hace lo mismo que el siguiente, aunque el siguiente
+// utiliza una función anónima (un tema más adelante)
+// botonesCerrar.forEach(boton => boton.addEventListener('click', function(){
+//   window.alert("adios");
+// }));
+
+/*
+let celdas = document.querySelectorAll('td');
+celdas.forEach(function(asd){
+  asd.addEventListener('click', function(){
+    console.log(this);
+    console.log("click");
+    // console.log(asd.innerHTML);
+  })
+  // console.log("click");
+});
+*/
+
+// celdas.forEach(c => c.addEventListener('click', function(){
+//   console.log("click.......");
+// }));
+
+/*
+console.log("Hola desde javascript");
+
+// DOM
+let container = document.querySelector(".container");
+console.log(container);
+console.log("Container: ", container);
+
+let tabla = document.querySelector(".tabla");
+console.log(tabla);
+
+let enlaces = document.querySelectorAll("a");
+console.log("Enlaces:");
+// enlaces.forEach(function(enlace){
+//   console.log(enlace);
+// });
+enlaces.forEach(enlace => {console.log(enlace)});
+// console.log(enlaces);
+*/
